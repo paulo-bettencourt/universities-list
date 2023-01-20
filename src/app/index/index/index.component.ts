0817388aa5e50ede4fd609bda8654f36da49f6bc
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-20 22:55:50 -0100

    hidden directive added

--- src/app/index/index/index.component.ts
+++ src/app/index/index/index.component.ts
@@ -26,18 +26,28 @@ export class IndexComponent {
     "name",
     "web_pages"
   ];
+  welcome = false;
+  hidePaginator = true;
+  noResults = true;
 
   constructor(private service: ApiService) {
     this.service.getCountries().subscribe(async (data: any) => {
       this.countries = await data
     })
-this.getUniversity();
   }
 
   getUniversity() {
     this.service.getUniversity(this.countryName?.value).subscribe({
       next: (data) => {
         this.universities = data;
+        this.welcome = true;
+        if(this.universities.length === 0) {
+          this.hidePaginator = true;
+          this.noResults = false;
+        } else {
+          this.hidePaginator = false;
+          this.noResults = true;
+        }
         this.dataSource = new MatTableDataSource(this.universities);
         this.dataSource.paginator = this.paginator;
         this.dataSourceWithPageSize.paginator = this.paginatorPageSize;
@@ -50,6 +60,14 @@ this.getUniversity();
 
   visitUniversityWebsite(web_pages: any) {
     window.open(web_pages, "mywindow","menubar=1,resizable=1,width=600,height=300");
+  }
+
+  clearData() {
+    // @ts-ignore
+    this.hidePaginator = true;
+    this.welcome = false;
+    this.noResults = true;
+    this.dataSource = new MatTableDataSource();
   }
 
 }
